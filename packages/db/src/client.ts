100909871a6d66a53fb617c60f1f6f798422103f
Kiel Green <kielgrn@gmail.com>
2026-04-11 18:59:29 -0400

    Use Prisma pg adapter for Vercel runtime

--- packages/db/src/client.ts
+++ packages/db/src/client.ts
@@ -1,4 +1,6 @@
+import { PrismaPg } from "@prisma/adapter-pg";
 import { PrismaClient } from "@prisma/client";
+import { Pool } from "pg";
 
 declare global {
   // eslint-disable-next-line no-var
@@ -8,10 +10,14 @@ declare global {
 export const prisma =
   global.__evolveEdgePrisma ??
   new PrismaClient({
+    adapter: new PrismaPg(
+      new Pool({
+        connectionString: process.env.DATABASE_URL
+      })
+    ),
     log: process.env.NODE_ENV === "development" ? ["warn", "error"] : ["error"]
   });
 
 if (process.env.NODE_ENV !== "production") {
   global.__evolveEdgePrisma = prisma;
 }
-
